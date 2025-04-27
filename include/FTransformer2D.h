888296e3cf5e3d6d9f53ab88b9b6436de0f3ca65
Sangwook Ryu <sangwook.ryu@mail.mcgill.ca>
2025-04-27 13:14:59 +0900

    update in LibFFourier
    introduction of Transformer2D class
    to perform 2-dimensional discrete Fourier transformations

---
+++ include/FTransformer2D.h
@@ -0,0 +1,63 @@
+#ifndef _FTRANSFORMER2D_H_
+#define _FTRANSFORMER2D_H_
+
+#include<string>
+#include"CNumber.h"
+
+namespace FFourier {
+
+class Transformer2D {
+  private :
+
+    int num_mesh_x_;
+    int num_mesh_y_;
+
+    CNumber **mesh_func_rx_ry_;
+    CNumber **mesh_func_kx_ky_;
+
+    bool initialized_;
+
+  public :
+
+    Transformer2D() {
+        initialized_ = false;
+
+        return;
+    }
+
+    ~Transformer2D() {
+        reset();
+
+        return;
+    }
+
+    void init(int num_in_mesh_x,
+              int num_in_mesh_y,
+              CNumber **mesh_in_func_rx_ry);
+    void init(int num_in_mesh_x,
+              int num_in_mesh_y,
+              CNumber (*ptr_in_func_rx_ry)(double,
+                                           double));
+
+    void make();
+
+    void shift(double dx, double dy);
+
+    void amplify(double fac);
+
+    void reset();
+
+    CNumber get_func_rx_ry(double x_in,
+                           double y_in);
+    CNumber get_func_rx_ry(int irx, int iry) {
+        return mesh_func_rx_ry_[irx][iry];
+    }
+
+    CNumber get_func_kx_ky(int ikx, int iky) {
+        return mesh_func_kx_ky_[ikx][iky];
+    }
+};
+
+} // end namespace FFourier
+
+#endif
---
+++ src/FTransformer2D.cpp
@@ -0,0 +1,285 @@
+#include<stdio.h>
+#include<math.h>
+#ifdef _OPENMP
+#include<omp.h>
+#endif
+#include"FTransformer1D.h"
+#include"FTransformer2D.h"
+
+namespace FFourier {
+
+void Transformer2D::init(int num_in_mesh_x,
+                         int num_in_mesh_y,
+                         CNumber **mesh_in_func_rx_ry) {
+    reset();
+
+    if (num_in_mesh_x < 2 ||
+        num_in_mesh_y < 2) {
+        return;
+    }
+
+    num_mesh_x_ = num_in_mesh_x;
+    num_mesh_y_ = num_in_mesh_y;
+
+    mesh_func_rx_ry_ = new CNumber *[num_mesh_x_];
+    mesh_func_kx_ky_ = new CNumber *[num_mesh_x_];
+    for (int irx = 0; irx < num_mesh_x_; irx++) {
+        mesh_func_rx_ry_[irx] = new CNumber[num_mesh_y_];
+        mesh_func_kx_ky_[irx] = new CNumber[num_mesh_y_];
+
+        for (int iry = 0; iry < num_mesh_y_; iry++) {
+            mesh_func_rx_ry_[irx][iry] =
+                mesh_in_func_rx_ry[irx][iry];
+        }
+    }
+
+    initialized_ = true;
+    make();
+
+    return;
+}
+
+void Transformer2D::init(int num_in_mesh_x,
+                         int num_in_mesh_y,
+                         CNumber (*ptr_in_func_rx_ry)(double,
+                                                      double)) {
+    reset();
+
+    if (num_in_mesh_x < 2 ||
+        num_in_mesh_y < 2) {
+        return;
+    }
+
+    num_mesh_x_ = num_in_mesh_x;
+    num_mesh_y_ = num_in_mesh_y;
+
+    mesh_func_rx_ry_ = new CNumber *[num_mesh_x_];
+    mesh_func_kx_ky_ = new CNumber *[num_mesh_x_];
+    for (int irx = 0; irx < num_mesh_x_; irx++) {
+        mesh_func_rx_ry_[irx] = new CNumber[num_mesh_y_];
+        mesh_func_kx_ky_[irx] = new CNumber[num_mesh_y_];
+
+        double x_now =
+            static_cast<double>(irx) /
+            static_cast<double>(num_mesh_x_);
+
+        for (int iry = 0; iry < num_mesh_y_; iry++) {
+            double y_now =
+                static_cast<double>(iry) /
+                static_cast<double>(num_mesh_y_);
+
+            mesh_func_rx_ry_[irx][iry] =
+                (*ptr_in_func_rx_ry)(x_now, y_now);
+        }
+    }
+
+    initialized_ = true;
+    make();
+
+    return;
+}
+
+void Transformer2D::make() {
+    if (!initialized_) {
+        return;
+    }
+
+    Transformer1D *ptr_dft_x =
+        new Transformer1D [num_mesh_x_]();
+
+    for (int irx = 0; irx < num_mesh_x_; irx++) {
+        ptr_dft_x[irx].init(num_mesh_y_,
+                            mesh_func_rx_ry_[irx]);
+    }
+
+    Transformer1D dft_all;
+    CNumber *mesh_func_x = new CNumber [num_mesh_x_];
+
+    for (int iky = 0; iky < num_mesh_y_; iky++) {
+        for (int irx = 0; irx < num_mesh_x_; irx++) {
+            mesh_func_x[irx] =
+                ptr_dft_x[irx].get_func_k(iky);
+        }
+
+        dft_all.init(num_mesh_x_,
+                     mesh_func_x);
+
+        for (int ikx = 0; ikx < num_mesh_x_; ikx++) {
+            mesh_func_kx_ky_[ikx][iky] =
+                dft_all.get_func_k(ikx);
+        }
+    }
+
+    delete [] mesh_func_x;
+
+    delete [] ptr_dft_x;
+
+    return;
+}
+
+void Transformer2D::shift(double dx, double dy) {
+    if (!initialized_) {
+        return;
+    }
+
+    CNumber zx_d_unit;
+    zx_d_unit[0] = cos(2. * M_PI * dx);
+    zx_d_unit[1] = sin(2. * M_PI * dx);
+    CNumber *list_zx_unit = new CNumber[num_mesh_x_];
+    list_zx_unit[0][0] = 1.;
+    list_zx_unit[0][1] = 0.;
+    for (int ikx = 1; ikx < num_mesh_x_; ikx++) {
+        list_zx_unit[ikx] =
+            list_zx_unit[ikx - 1] * zx_d_unit;
+    }
+
+    CNumber zy_d_unit;
+    zy_d_unit[0] = cos(2. * M_PI * dy);
+    zy_d_unit[1] = sin(2. * M_PI * dy);
+    CNumber *list_zy_unit = new CNumber[num_mesh_y_];
+    list_zy_unit[0][0] = 1.;
+    list_zy_unit[0][1] = 0.;
+    for (int iky = 1; iky < num_mesh_y_; iky++) {
+        list_zy_unit[iky] =
+            list_zy_unit[iky - 1] * zy_d_unit;
+    }
+
+    #ifdef _OPENMP
+    #pragma omp parallel
+    {  // parallel code begins
+    #endif
+        #ifdef _OPENMP
+        int n_thread = omp_get_num_threads();
+        int tid = omp_get_thread_num();
+        #endif
+
+        for (int ikx = 0; ikx < num_mesh_x_; ikx++) {
+            #ifdef _OPENMP
+            if (ikx % n_thread != tid) {
+                continue;
+            }
+            #endif
+
+            for (int iky = 0; iky < num_mesh_y_; iky++) {
+                mesh_func_kx_ky_[ikx][iky] =
+                    mesh_func_kx_ky_[ikx][iky] /
+                    (list_zx_unit[ikx] * list_zy_unit[iky]);
+            }
+        }
+
+        #ifdef _OPENMP
+        // syncronize threads
+        #pragma omp barrier
+        #endif
+
+        for (int irx = 0; irx < num_mesh_x_; irx++) {
+            #ifdef _OPENMP
+            if (irx % n_thread != tid) {
+                continue;
+            }
+            #endif
+
+            double x_now =
+                static_cast<double>(irx) /
+                static_cast<double>(num_mesh_x_);
+
+            for (int iry = 0; iry < num_mesh_y_; iry++) {
+                double y_now =
+                    static_cast<double>(iry) /
+                    static_cast<double>(num_mesh_y_);
+
+                mesh_func_rx_ry_[irx][iry] =
+                    get_func_rx_ry(x_now, y_now);
+            }
+        }
+    #ifdef _OPENMP
+    }  // parallel code ends
+    #endif
+
+    delete [] list_zx_unit;
+    delete [] list_zy_unit;
+
+    return;
+}
+
+void Transformer2D::amplify(double fac) {
+    if (!initialized_) {
+        return;
+    }
+
+    for (int irx = 0; irx < num_mesh_x_; irx++) {
+        for (int iry = 0; iry < num_mesh_y_; iry++) {
+            mesh_func_rx_ry_[irx][iry] =
+                fac * mesh_func_rx_ry_[irx][iry];
+            mesh_func_kx_ky_[irx][iry] =
+                fac * mesh_func_kx_ky_[irx][iry];
+        }
+    }
+
+    return;
+}
+
+void Transformer2D::reset() {
+    if (!initialized_) {
+        return;
+    }
+
+    for (int irx = 0; irx < num_mesh_x_; irx++) {
+        delete [] mesh_func_rx_ry_[irx];
+        delete [] mesh_func_kx_ky_[irx];
+    }
+
+    delete [] mesh_func_rx_ry_;
+    delete [] mesh_func_kx_ky_;
+
+    num_mesh_x_ = 0;
+    num_mesh_y_ = 0;
+
+    initialized_ = false;
+
+    return;
+}
+
+CNumber Transformer2D::get_func_rx_ry(double x_in,
+                                      double y_in) {
+    CNumber zx_in_unit;
+    zx_in_unit[0] = cos(2. * M_PI * x_in);
+    zx_in_unit[1] = sin(2. * M_PI * x_in);
+    CNumber *list_zx_unit = new CNumber[num_mesh_x_];
+    list_zx_unit[0][0] = 1.;
+    list_zx_unit[0][1] = 0.;
+    for (int ikx = 1; ikx < num_mesh_x_; ikx++) {
+        list_zx_unit[ikx] =
+            list_zx_unit[ikx - 1] * zx_in_unit;
+    }
+
+    CNumber zy_in_unit;
+    zy_in_unit[0] = cos(2. * M_PI * y_in);
+    zy_in_unit[1] = sin(2. * M_PI * y_in);
+    CNumber *list_zy_unit = new CNumber[num_mesh_y_];
+    list_zy_unit[0][0] = 1.;
+    list_zy_unit[0][1] = 0.;
+    for (int iky = 1; iky < num_mesh_y_; iky++) {
+        list_zy_unit[iky] =
+            list_zy_unit[iky - 1] * zy_in_unit;
+    }
+
+    CNumber cnum_ret;
+    cnum_ret[0] = 0.;
+    cnum_ret[1] = 0.;
+
+    for (int ikx = 0; ikx < num_mesh_x_; ikx++) {
+        for (int iky = 0; iky < num_mesh_y_; iky++) {
+            cnum_ret = cnum_ret +
+                (mesh_func_kx_ky_[ikx][iky] *
+                 list_zx_unit[ikx] * list_zy_unit[iky]);
+        }
+    }
+
+    delete [] list_zx_unit;
+    delete [] list_zy_unit;
+
+    return cnum_ret / static_cast<double>(num_mesh_x_ * num_mesh_y_);
+}
+
+} // end namespace FFourier
